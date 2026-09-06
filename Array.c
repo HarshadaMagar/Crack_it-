17cc473c24072a51d9c4b9cba87e4da719bc8c5f
Harshada Magar <Harshadamagar2895@gmail.com>
2026-09-07 01:26:04 +0530

    Add Array to print student marks

---
+++ Array.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int marks[5] = {56,67,78,89,90};
+    int i;
+
+    for(i = 0; i < 5; i++)
+    {
+        printf("%d\n", marks[i]);
+    }
+
+    return 0;
+}
